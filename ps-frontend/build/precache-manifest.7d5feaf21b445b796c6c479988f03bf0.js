060122e2ed2e300be52872a4aa47df92f84abd1f
Jrowan19 <Jrowan19>
2019-12-15 14:43:16 +0000

    added build for netlify

---
+++ ps-frontend/build/precache-manifest.7d5feaf21b445b796c6c479988f03bf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48111c9215a4e8dd8e658a3083b7f90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f71de2f53c2400ee879f",
+    "url": "/static/css/2.51f4a81e.chunk.css"
+  },
+  {
+    "revision": "5dc1af99471c69ee9d7b",
+    "url": "/static/css/main.741c1543.chunk.css"
+  },
+  {
+    "revision": "f71de2f53c2400ee879f",
+    "url": "/static/js/2.1cc4fc5e.chunk.js"
+  },
+  {
+    "revision": "5055ecf597a48448f4a23a605c8a5965",
+    "url": "/static/js/2.1cc4fc5e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5dc1af99471c69ee9d7b",
+    "url": "/static/js/main.e8709ce6.chunk.js"
+  },
+  {
+    "revision": "175cc5d869ea28b7060e",
+    "url": "/static/js/runtime-main.dcca27f6.js"
+  }
+]);
